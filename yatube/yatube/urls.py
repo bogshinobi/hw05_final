82197a8fbe0071b62b1759a0d13ba65f75d11f1c
Ayur Urbakhanov <ayururbakhanov@gmail.com>
2022-04-13 21:25:40 +0300

    static

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -21,7 +21,7 @@ if settings.DEBUG:
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
     urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+        settings.STATIC_URL, document_root=settings.STATICFILES_DIRS
     )
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
 
